3f099147d028fef4496c17050cb65ea9b4f15cd1
easa <eisanodehi@gmail.com>
2019-10-14 13:42:07 +0330

    add index file

---
+++ index.js
@@ -0,0 +1,10 @@
+function app() {
+  return {
+    status: 'under construction!',
+    version: '0.0.4',
+    auther: 'github.com/easa',
+    desc: 'The NPM package to perform statistical models on any text and extract specific features'
+  }
+}
+
+module.exports = app;
