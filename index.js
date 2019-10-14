6fe31e87dcb3f952a402273159f3ef645895dcbd
easa <eisanodehi@gmail.com>
2019-10-14 14:09:50 +0330

    update index - the main function was extra

--- index.js
+++ index.js
@@ -2,16 +2,12 @@
 
 var app = require('./app')
 
-function main() {
-  return {
-    status: 'version 1!',
-    version: '1.0.0',
-    auther: 'github.com/easa',
-    desc: 'The NPM package to perform statistical models on any text and extract specific features',
-    featuresList: app({ isFeatureNameRequired: true }),
-    featuresCount: app({ isFeatureCountRequired: true }),
-    extractFeatures: (input) => app({ input: input, isRawRequired: true })
-  }
+module.exports = {
+  status: 'version 1!',
+  version: '1.0.0',
+  auther: 'github.com/easa',
+  desc: 'The NPM package to perform statistical models on any text and extract specific features',
+  featuresList: app({ isFeatureNameRequired: true }),
+  featuresCount: app({ isFeatureCountRequired: true }),
+  extractFeatures: (input) => app({ input: input, isRawRequired: true })
 }
-
-module.exports = main;
