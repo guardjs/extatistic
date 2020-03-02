676a22e383a56c465aa5a902376b60464cff4a88
easa <eisanodehi@gmail.com>
2020-03-02 17:21:08 +0330

    correction GPR

--- dist/cmd/count.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-exports.command = ['count', 'total'];
-exports.desc = 'Show total count of registered features.';
-exports.builder = {};
-
-exports.handler = function (argv) {
-  console.log(argv.app.calculateFeatureCount());
-};
--- dist/cmd/list.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-exports.command = ['list', 'features'];
-exports.desc = 'Show a list of registered features.';
-exports.builder = {};
-
-exports.handler = function (argv) {
-  console.log(argv.app.getListOfFeaturesName());
-};
