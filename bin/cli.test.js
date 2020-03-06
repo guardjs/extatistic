1584076393dba03a4f1040add0082211cf87891f
easa <eisanodehi@gmail.com>
2020-03-06 21:37:34 +0330

    update cli.test.js"

--- bin/cli.test.js
+++ bin/cli.test.js
@@ -1,9 +1,11 @@
 
 import { execFile as exec } from 'child_process';
+import { doesNotReject } from 'assert';
 
 it('should start with offset [--from, -f]', () => {
   exec('node', ['./cli', '--version'], (error, stdout) => {
-      if (error) { throw error }
-      console.log(stdout);
+    if (error) { throw error }
+    console.log(stdout)
+    done()
   })
 })
