30461a6368fd43ecaf0434f1826e31c6320ba742
easa <eisanodehi@gmail.com>
2020-03-07 04:06:16 +0330

    Update cli.js mock

--- bin/cli.js
+++ bin/cli.js
@@ -1,13 +1,13 @@
 #! /usr/bin/env node
 console.log("extatistic - extract")
 
-// import fs from 'fs'
+import * as fs from 'fs'
 import app from '../syntaxFeatures'
 import filehandler from '../fileHandler'
 
 require('yargs')
   .middleware(argv => {
-    argv.fs = {}
+    argv.fs = fs
     argv.app = app
     argv.file = filehandler
     return argv
--- bin/cli.test.js
+++ bin/cli.test.js
@@ -3,6 +3,9 @@ import { execFile as exec } from 'child_process';
 import { doesNotReject } from 'assert';
 
 it('should start with offset [--from, -f]', () => {
+  jest.mock('fs')
+  jest.mock('../syntaxFeatures')
+  jest.mock('../fileHandler')
   exec('node', ['./bin/cli', '--version'], (error, stdout) => {
     if (error) { throw error }
     console.log(stdout)
