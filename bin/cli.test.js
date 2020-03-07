a0e0adf0d405c922a32521bcb8887255d85e06eb
easa <eisanodehi@gmail.com>
2020-03-07 04:16:04 +0330

    Update cli.test.js - await async - done

--- bin/cli.test.js
+++ bin/cli.test.js
@@ -1,11 +1,11 @@
 
 import { execFile as exec } from 'child_process';
 
-it('should start with offset [--from, -f]', () => {
+test('should start with offset [--from, -f]', async done => {
   jest.mock('fs')
   jest.mock('../syntaxFeatures')
   jest.mock('../fileHandler')
-  exec('node', ['./bin/cli', '--version'], (error, stdout) => {
+  await exec('node', ['./bin/cli', '--version'], (error, stdout) => {
     if (error) { throw error }
     console.log(stdout)
     done()
