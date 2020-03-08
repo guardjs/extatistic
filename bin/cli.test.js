e0e1e8e7aec30fb721c7037195a9cf48564321cf
easa <eisanodehi@gmail.com>
2020-03-08 03:33:58 +0330

    unit test cli - mock functions to prevent error

--- bin/cli.test.js
+++ bin/cli.test.js
@@ -3,6 +3,8 @@ import { execFile as exec } from 'child_process';
 
 // it's just a unit-test to see if yargs is running and cli is working
 test('should start and show version', async done => {
+  jest.mock('../extractor')
+  jest.mock('../fileHandler')
   await exec('node', ['./bin/cli', '--version'], (error, stdout) => {
     if (error) { throw error }
     console.log(`--version ${stdout}`.replace(/\n/g, ''))
